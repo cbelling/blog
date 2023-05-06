a3de86b5ad38b4b242496e6fd792290c339c6cd2
Charles Bellinger <charlesbellinger@Charless-MBP.attlocal.net>
2023-05-06 12:53:50 -0400

    meg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
 
 const Header = () => {
@@ -7,6 +7,14 @@ const Header = () => {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
+  useEffect(() => {
+    if (showMenu) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+  }, [showMenu]);
 
   const renderNavLinks = () => (
     <>
@@ -27,21 +35,25 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-between mb-10 px-4">
-      <p className="text-xl font-bold text-center">Austin Bellinger</p>
-      <div className="relative md:hidden" style={{ marginLeft: 'auto' }}>
-        <button
-          onClick={toggleMenu}
-          className="text-xl bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
-        >
-          {showMenu ? <FiX /> : <FiMenu />}
-        </button>
-        <nav
-          className={`absolute right-0 mt-2 bg-white text-lg flex flex-col md:flex-row md:items-center md:justify-between md:w-auto ${
-            showMenu ? 'block' : 'hidden'
-          }`}
-        >
-          {renderNavLinks()}
-        </nav>
+    <p className="text-xl font-bold text-center">Austin Bellinger</p>
+    <div className="relative md:hidden" style={{ marginLeft: 'auto' }}>
+      <button
+        onClick={toggleMenu}
+        className="text-xl bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded relative z-10"
+      >
+        {showMenu ? <FiX /> : <FiMenu />}
+      </button>
+      <nav
+        className={`fixed top-1/2 left-1/2 h-full w-full items-center justify-center flex z-0 transform -translate-x-1/2 -translate-y-1/2 rounded bg-gray-100 text-lg flex flex-col md:flex-row md:items-center md:justify-between ${
+          showMenu ? 'block' : 'hidden'
+        }`}
+        style={{ transition: 'opacity 0.3s ease-in-out' }}
+      >
+        {renderNavLinks()}
+      </nav>
+
+
+
       </div>
       <nav className="hidden md:flex md:items-center md:justify-between md:w-auto">
         {renderNavLinks()}
